411de78432ce505d35816509282ee4d682983e76
MidoriyaHero <buitrungtin0105@gmail.com>
2024-09-25 14:43:55 +0700

    add lecture 1

--- Lecture 1/main.py
+++ Lecture 1/main.py
@@ -1,43 +1,23 @@
 from fastapi import FastAPI
-from typing import List
-from models import User, Gender, Role
-from uuid import uuid4, UUID
-from fastapi.responses import HTMLResponse
-
 
 app = FastAPI()
-db: List[User] =[
-    User(id = uuid4(), 
-         name = 'Tin',
-         gender = Gender.male,
-         role = [Role.admin],), 
-
-    User(id = uuid4(), 
-         name = 'Tindeptrain',
-         gender = Gender.female,
-         role = [Role.user, Role.admin],),
-]
-
-@app.get('/',response_class = HTMLResponse)
-def root():
-    return """
-    <html>
-        <head>
-            <title>Home</title>
-        </head>
-        <body>
-            <h1>Fast API</h1>
-            <p>Go to: 127.0.0.1:8000/docs</p>
-        </body>
-    </html>
-    """
-
-@app.get('/api/users/v1')
-async def fetch_users():
-    return db
 
 
-@app.post('/api/users/v1')
-async def add_user(user: User):
-    db.append(user)
-    return 'user added'
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/put")
+async def put():
+    return {"message": "this is put method"}
+
+
+@app.get("/post")
+async def post():
+    return {"message": "this is post method"}
+
+
+@app.get("/delete")
+async def delete():
+    return {"message": "this is delete method"}
--- Lecture 1/readme.md
+++ Lecture 1/readme.md
@@ -1,1 +1,19 @@
 # Lecture 1
+## Description:
+This lecture will describe put get post delete method in FastAPI
+
+When building APIs, you normally use these specific HTTP methods to perform a specific action.
+
+Normally you use:
+
+* POST: to create data.
+* GET: to read data.
+* PUT: to update data.
+* DELETE: to delete data.
+
+## USE THIS COMMAND TO START APP SERVER:
+```
+uvicorn main:app --reload
+```
+
+YOU CAN FIND MORE FROM THIS [LINK](https://fastapi.tiangolo.com/tutorial/first-steps/#alternative-api-docs)
